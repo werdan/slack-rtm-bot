a795fe1cfc4532baf8e75ae540d71501291b0583
Andriy Samilyak <samilyak@gmail.com>
2017-07-11 10:48:43 +0200

    Retry on fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from rtmbot import RtmBot
 import os
+import traceback
 
 config = {
 	'DAEMON' : False,
@@ -10,6 +11,11 @@ config = {
 bot = RtmBot(config)
 
 while True:
-	bot.start()
+	try:
+		bot.start()
 	# Occassionally we got https://api.slack.com/events/team_migration_started this event, that closes websocket
+	except:
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+		print ''.join('!! ' + line for line in lines)
 	time.sleep(5)
